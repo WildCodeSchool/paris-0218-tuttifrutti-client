e7a09f5c9e4089089fdc8341bf563f6f6a34ae1d
Ludivine Pellissier <ludivine.pellissier@gmail.com>
2018-06-04 11:39:00 +0200

    install lint + travis

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import Home from './components/Home.js'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-       <Home />
+        <Home />
       </div>
     )
   }
 }
 
-export default App;
+export default App
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const Button = ({text}, req) => (
-    <button>{text}</button>
+  <button>{text}</button>
 )
 
-export default Button 
+export default Button
