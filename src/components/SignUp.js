f8ee728860ac4af6195706303488c16392bd467e
Novyrok <novyrok@localhost.localdomain>
2018-06-05 17:57:54 +0200

    create input CSS

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,6 +4,7 @@ import Login from './Login.js'
 import Button from './Button.js'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import LawyerTitleSpace from './TitleSpace.js'
+import '../style/SignUp.css'
 
 class SignUp extends React.Component {
     state={
@@ -52,31 +53,46 @@ class SignUp extends React.Component {
       return (
         <div>
           <LawyerTitleSpace />
-          <form  ClassName= 'form' onSubmit={this.HandleSubmit}>
-            <label htmlFor = "firstname"> Nom </label>
-            <input type ="text" name="firstName" id="firstName" onChange={this.UpdateField} />
-            <label htmlFor = "lastName"> Prenom </label>
-            <input type ="text" name="lastName" id="lastName" onChange={this.UpdateField}/>
-            <label htmlFor = "email"> Email </label>
-            <input type="email" name="email" id="email" onChange={this.UpdateField}/>
-            <label htmlFor = "phone"> Telephone </label>
-            <input type ="text" name="phone" id="phone" onChange={this.UpdateField} />
-            <label htmlFor = "cabinet">Nom du Cabinet </label>
-            <input type ="text" name="cabinet" id="cabinet" onChange={this.UpdateField} />
-            <label htmlFor = "toque"> Numéro de Toque </label>
-            <input type ="text" name="toque" id="toque" onChange={this.UpdateField} />        
-            <label htmlFor = "address"> Adresse </label>
-            <input type ="text" name="address" id="address" onChange={this.UpdateField} />
-            <label htmlFor = "zipCode"> Code Postal </label>
-            <input type ="text" name="city" id="city" onChange={this.UpdateField} />        
-            <label htmlFor = "city"> Ville </label>
-            <input type ="text" name="zipCode" id="zipCode" onChange={this.UpdateField} />     
-            <label htmlFor = "field"> Domaine </label>
-            <input type ="text" name="field" id="field" onChange={this.UpdateField} />
-            <label htmlFor = "password"> Mot de passe </label>
-            <input type ="password" name="password" id="password" onChange={this.UpdateField} />
-            <label htmlFor = "passwordComfirm"> Confirmer le mot de passe </label>
-            <input type ="password" name="passwordComfirm" id="passwordComfirm" />
+          <form  className='form' onSubmit={this.HandleSubmit}>
+            <div className='aligne'>
+            <input type ="text" name="firstName" placeholder="Nom" id="firstName" onChange={this.UpdateField} /> 
+            <input type ="text" name="lastName" placeholder="Prenom" id="lastName" onChange={this.UpdateField}/>
+            </div>
+            <div ClassName='aligne'>            
+            <input type="email" name="email" placeholder="Email" id="email" onChange={this.UpdateField}/>
+             
+            <input type ="text" name="phone" placeholder="Telephone" id="phone" onChange={this.UpdateField} />
+            </div>
+            <div>
+            
+            <input type ="text" name="cabinet" placeholder="Nom du Cabinet" id="cabinet" onChange={this.UpdateField} />
+            
+            <input type ="text" name="toque" placeholder="Numéro de Toque" id="toque" onChange={this.UpdateField} />        
+            </div>
+            <div>
+            
+            <input type ="text" name="address" placeholder="Adresse" id="address" onChange={this.UpdateField} />
+            </div>
+            <div>
+            
+            <input type ="text" name="city" placeholder="Ville" id="city" onChange={this.UpdateField} />        
+            
+            <input type ="text" name="zipCode" placeholder="Code Postal" id="zipCode" onChange={this.UpdateField} />
+            </div>
+            <div>     
+            <input type ="text" name="domaine" placeholder="Domaine" id="domaine" onChange={this.UpdateField} />
+            <select> 
+              <option value="1">droit</option>
+              <option value="2">droit2</option>
+              <option value="3">droit3</option>
+              <option value="4">droit4</option>
+              <option value="5">droit5</option>
+            </select>
+            </div>
+            <div>
+            <input type ="password" name="password" placeholder="Mot de passe" id="password" onChange={this.UpdateField} />
+            <input type ="password" name="passwordComfirm" placeholder="Confimer le mot de passe" id="passwordComfirm" />
+            </div>
             <button type ="submit">S'incrire</button>
             <p> Déjà inscrit ? <Link to={`/login`}> <a href text='login' />Connectez-vous</Link></p>
             <Route path='/login' component={Login} /> 
