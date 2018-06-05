e67e70d286e9fce229d33506e0b3fb6c6ac2a393
Baloulee <valerychin@MacBook-Pro-de-Baloulee.local>
2018-06-05 21:16:36 +0200

    css realisé thx ludidi

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -53,50 +53,45 @@ class SignUp extends React.Component {
       return (
         <div>
           <LawyerTitleSpace />
-          <form  className='form' onSubmit={this.HandleSubmit}>
-            <div className='aligne'>
-            <input type ="text" name="firstName" placeholder="Nom" id="firstName" onChange={this.UpdateField} /> 
-            <input type ="text" name="lastName" placeholder="Prenom" id="lastName" onChange={this.UpdateField}/>
-            </div>
-            <div ClassName='aligne'>            
-            <input type="email" name="email" placeholder="Email" id="email" onChange={this.UpdateField}/>
-             
-            <input type ="text" name="phone" placeholder="Telephone" id="phone" onChange={this.UpdateField} />
-            </div>
-            <div>
-            
-            <input type ="text" name="cabinet" placeholder="Nom du Cabinet" id="cabinet" onChange={this.UpdateField} />
-            
-            <input type ="text" name="toque" placeholder="Numéro de Toque" id="toque" onChange={this.UpdateField} />        
-            </div>
-            <div>
-            
-            <input type ="text" name="address" placeholder="Adresse" id="address" onChange={this.UpdateField} />
-            </div>
-            <div>
-            
-            <input type ="text" name="city" placeholder="Ville" id="city" onChange={this.UpdateField} />        
-            
-            <input type ="text" name="zipCode" placeholder="Code Postal" id="zipCode" onChange={this.UpdateField} />
-            </div>
-            <div>     
-            <input type ="text" name="domaine" placeholder="Domaine" id="domaine" onChange={this.UpdateField} />
-            <select> 
-              <option value="1">droit</option>
-              <option value="2">droit2</option>
-              <option value="3">droit3</option>
-              <option value="4">droit4</option>
-              <option value="5">droit5</option>
-            </select>
-            </div>
-            <div>
-            <input type ="password" name="password" placeholder="Mot de passe" id="password" onChange={this.UpdateField} />
-            <input type ="password" name="passwordComfirm" placeholder="Confimer le mot de passe" id="passwordComfirm" />
-            </div>
-            <button type ="submit">S'incrire</button>
-            <p> Déjà inscrit ? <Link to={`/login`}> <a href text='login' />Connectez-vous</Link></p>
-            <Route path='/login' component={Login} /> 
-          </form>
+          <div className="formSignUp">
+            <form className="form" onSubmit={this.HandleSubmit}>
+              <div>
+                <input type ="text" name="firstName" placeholder="Nom" id="firstName" onChange={this.UpdateField} /> 
+                <input type ="text" name="lastName" placeholder="Prénom" id="lastName" onChange={this.UpdateField} />
+              </div>
+              <div>            
+                <input type="email" name="email" placeholder="Email" id="email" onChange={this.UpdateField} />
+                <input type ="text" name="phone" placeholder="Téléphone" id="phone" onChange={this.UpdateField} />
+              </div>
+              <div>
+                <input type ="text" name="cabinet" placeholder="Nom du cabinet (facultatif)" id="cabinet" onChange={this.UpdateField} />
+                <input type ="text" name="toque" placeholder="N° de toque (facultatif)" id="toque" onChange={this.UpdateField} />        
+              </div>
+              <div>
+                <input type ="text" name="address" placeholder="Adresse" id="address" onChange={this.UpdateField} />
+              </div>
+              <div>
+                <input type ="text" name="zipCode" placeholder="Code postal" id="zipCode" onChange={this.UpdateField} />
+                <input type ="text" name="city" placeholder="Ville" id="city" onChange={this.UpdateField} />        
+              </div>
+              <div>     
+                <select name="domaine" placeholder="Domaine" id="domaine" onChange={this.UpdateField} > 
+                <option value="" disabled selected>Domaine</option>
+                <option value="1">droit2</option>
+                <option value="2">droit3</option>
+                <option value="3">droit4</option>
+                <option value="5">droit5</option>
+              </select>
+              </div>
+              <div>
+                <input type ="password" name="password" placeholder="Mot de passe" id="password" onChange={this.UpdateField} />
+                <input type ="password" name="passwordComfirm" placeholder="Confimer le mot de passe" id="passwordComfirm" />
+              </div>
+              <button type ="submit">S'inscrire</button>
+            </form>
+          </div>
+              <p>Déjà inscrit ? <Link to={`/login`}>Connectez-vous</Link></p>
+              <Route path='/login' component={Login} />
         </div>
       )
     }
