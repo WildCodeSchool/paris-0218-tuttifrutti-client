239cd7408aff2c565ca7184972768573988d4538
fenrisl <daniel.deutsch25@gmail.com>
2018-06-08 10:15:21 +0200

    protected routes

--- src/components/Login2.js
+++ src/components/Login2.js
@@ -2,23 +2,24 @@ import React from 'react'
 import  { Redirect } from 'react-router-dom'
 
 class Login extends React.Component {
-    state = {
-    }
-
-
-
     
     componentWillMount() {
         const token = localStorage.getItem('token')
-        if (token === null) {window.location.replace("http://stackoverflow.com")}
+        if (token === null) {window.location.replace("/login")}
         else {
         fetch(`http://localhost:3030/secure`, {
             headers: {
                 Authorization: `Bearer ${token}`
             },
-            // credentials: 'include',
         } 
-    )}}
+    ).then(response => {
+        response.json()
+            .then(responseJson => {console.log(responseJson)
+                if (responseJson === 'notlogged' ) {console.log('blop')
+                window.location.replace("/login")}
+            })
+    })
+}}
 
     render() {
         return (
