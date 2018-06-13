c26d34946a3cc8c199d19a171dfe095387ddbf00
Ludivine Pellissier <ludivine.pellissier@gmail.com>
2018-06-13 10:41:55 +0200

    fix install lint + fix lint

--- src/App.js
+++ src/App.js
@@ -13,5 +13,4 @@ class App extends Component {
   }
 }
 
-
 export default App
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const PageTitle = ({title}) => {
-    return (
+  return (
     <div>
-        <h1>Espace avocat</h1>
-        <h2>{title}</h2>
+      <h1>Espace avocat</h1>
+      <h2>{title}</h2>
     </div>
-    )
+  )
 }
 
 export default PageTitle
