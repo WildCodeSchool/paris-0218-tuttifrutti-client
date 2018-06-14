abf6332e5b2b655cb951baad376556eb0578bd77
Ludivine Pellissier <ludivine.pellissier@gmail.com>
2018-06-14 21:03:01 +0200

    show each mission on all missions page

--- src/containers/AllMissions.js
+++ src/containers/AllMissions.js
@@ -8,38 +8,44 @@ import MissionDeadline from '../components/MissionDeadline.js'
 import './style/AllMissions.css'
 
 class AllMissions extends React.Component {
-    state = []
+  state = {
+    allMissions: []
+  }
 
-    componentDidMount() {
-        axios.get(`http://localhost:3030/missions`)
-            .then(console.log('ok'))
-            .then((res) => {
-                this.setState({
-                    id: res.data._id,
-                    name: res.data.name,
-                    field: res.data.field,
-                    deadline: res.data.deadline,
-                })
-                console.log(this.state)
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+  componentDidMount() {
+    axios.get(`http://localhost:3030/missions`)
+      .then((res) => {
+        this.setState({ allMissions: res.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  render() {
+
+    const eachMission = mission => {
+      return (
+        <div className='each-mission-container'>
+          <MissionTitle text={mission.name} />
+          <MissionId text={mission.id} />
+          <MissionField text={mission.field} />
+          <MissionDeadline text={mission.deadline} />
+          <div className='button-mission-more'>
+            <Button>Voir le détail</Button>
+          </div>
+        </div>
+      )
     }
 
-    render() {
-        return (
-            <div>
-                <MissionTitle text={this.state.name} />
-                <MissionId text={this.state.id} />
-                <MissionField text={this.state.field} />
-                <MissionDeadline text={this.state.deadline} />
-                <div className='button-mission-more'>
-                    <Button>Ajouter un document</Button>
-                </div>
-            </div>
-        )
-    }
+    const showEachMission = this.state.allMissions.map(mission => eachMission(mission))
+
+    return (
+      <div className='all-missions-container'>
+        {showEachMission}
+      </div>
+    )
+  }
 }
 
 export default AllMissions
