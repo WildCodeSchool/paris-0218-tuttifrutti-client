f1b4333f5cec1ccd3d0751bb39edafdc8dab88e4
Baloulee <valerychin@MacBook-Pro-de-Baloulee.local>
2018-06-14 16:50:02 +0200

    signupstudent realisé

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 import './style/App.css'
 import Home from './containers/Home.js'
+import SignUpStudent from './components/SignUpStudent.js'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <Home />
+        <SignUpStudent />
       </div>
     )
   }
