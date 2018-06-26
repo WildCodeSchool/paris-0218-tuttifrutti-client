ecd9cc0d9e1dc13b24dbd7ef7f415ab6d49407ee
Goosefr <yann.coulleau@gmail.com>
2018-06-26 08:40:22 +0200

    added the system of email confirmation for the new users / subscribers. Romain will check where we need to add the Random for the token in the Database

--- src/components/sendmail.js
+++ src/components/sendmail.js
@@ -1,29 +1,70 @@
-const nodemailer = require('nodemailer');
-
-// create reusable transporter object using SMTP transport
-const
- transporter = nodemailer.createTransport({
-    service: 'Gmail',
+Const express=require('express');
+Const nodemailer = require("nodemailer");
+Const app=express();
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+Const smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
     auth: {
-        user: 'gmail.user@gmail.com',
-        pass: 'userpass'
+        user: "Your Gmail ID", //this is where we need to add the credentials provided by Litta
+        pass: "Gmail Password" //this is where we need to add the credentials provided by Litta
     }
 });
+Const rand,mailOptions,host,link;
+/*------------------SMTP Over-----------------------------*/
 
-// setup e-mail data with unicode symbols
-var mailOptions = {
-    from: 'Tutti Frutti ✔ <tuttifrutti@gmail.com>', // sender address
-    to: 'user@tuttifrutti.com, user2@tuttifrutti.com', // list of receivers always separated by ","
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ✔', // plaintext body
-    html: '<b>Hello world ✔</b>' // html body
-};
+/*------------------Routing Started ------------------------*/
 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-    }else{
-        console.log('Message sent: ' + info.response);
+app.get('/',function(req,res){
+    res.sendfile('index.html');
+});
+app.get('/send',function(req,res){
+        //rand=Math.floor((Math.random() * 100) + 54); @Romain, check with Daniel where we need to add the Random in the BDD
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
     }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
+
+app.get('/verify',function(req,res){
+console.log(req.protocol+":/"+req.get('host'));
+if((req.protocol+"://"+req.get('host'))==("http://"+host))
+{
+    console.log("Domain is matched. Information is from Authentic email");
+    if(req.query.id==rand)
+    {
+        console.log("email is verified");
+        res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+    }
+    else
+    {
+        console.log("email is not verified");
+        res.end("<h1>Bad Request</h1>");
+    }
+}
+else
+{
+    res.end("<h1>Request is from unknown source");
+}
+});
+
+/*--------------------Routing Over----------------------------*/
+
+app.listen(3000,function(){
+    console.log("Express Started on Port 3000");
 });
