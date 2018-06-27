725ef7751e27a60c718eee825ab7a0184a354ebe
Ludivine Pellissier <ludivine.pellissier@gmail.com>
2018-06-27 18:14:54 +0200

    fix click zone on homepagelawyer

--- src/containers/HomeLawyerMissions.js
+++ src/containers/HomeLawyerMissions.js
@@ -5,12 +5,12 @@ import './style/HomeLawyerMissions.css'
 
 const HomeLawyerMissions = () => (
   <div className='home-lawyer-missions'>
-    <div className='home-lawyer-currentmissions'>
-      <Link to={`/missions`}><MissionTitle className='lawyer-mission-title' text='Missions en cours' /></Link>
-    </div>
-    <div className='home-lawyer-oldmissions'>
-      <Link to={`/oldmissions`}><MissionTitle className='lawyer-mission-title' text='Historique des missions' /></Link>
-    </div>
+    <Link  className='home-lawyer-missions-linkto' to={`/missions`}><div className='home-lawyer-currentmissions'>
+      <MissionTitle text='Missions en cours' />
+    </div></Link>
+    <Link className='home-lawyer-missions-linkto' to={`/oldmissions`}><div className='home-lawyer-oldmissions'>
+      <MissionTitle text='Historique des missions' />
+    </div></Link>
   </div>
 )
 
