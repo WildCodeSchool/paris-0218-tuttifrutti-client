bb8be8cb2c49312cda1741a10ce70117aa29fa63
Lor Chtg <lorraine.chastang@gmail.com>
2018-06-28 18:48:20 +0200

    attempt to update data

--- src/components/Parameters.js
+++ src/components/Parameters.js
@@ -1,22 +1,12 @@
 import React from 'react'
 import { userInfo } from '../User.js'
-// import axios from 'axios'
+import axios from 'axios'
 import Button from './Button.js'
 import './style/Parameters.css'
 
 class Parameters extends React.Component {
   state = {
-    // email: '',
-    // password: '',
-    // firstName: '',
-    // lastName: '',
-    // cabinet: '',
-    // phone: '',
-    // address: '',
-    // city: '',
-    // zipCode: '',
-    // toque: '',
-    // field: ''
+    lawyer: {},
     displayInfo: 'block',
     displayForm: 'none'
   }
@@ -24,35 +14,37 @@ class Parameters extends React.Component {
   componentDidMount () {
     userInfo().then(res =>
       this.setState({
-        email: res.email,
-        password: res.password,
-        firstName: res.firstName,
-        lastName: res.lastName,
-        cabinet: res.cabinet,
-        phone: res.phone,
-        address: res.address,
-        city: res.city,
-        zipCode: res.zipCode,
-        toque: res.toque,
-        field: res.field
+        lawyer: {
+          email: res.email,
+          password: res.password,
+          firstName: res.firstName,
+          lastName: res.lastName,
+          cabinet: res.cabinet,
+          phone: res.phone,
+          address: res.address,
+          city: res.city,
+          zipCode: res.zipCode,
+          toque: res.toque,
+          field: res.field
+        }
       }))
   }
 
-  UpdateField = event => { this.setState({ [event.target.name]: event.target.value }) }
+  UpdateField = event => { this.setState({ lawyer: {[event.target.name]: event.target.value} }) }
 
   HandleSubmit = event => {
     event.preventDefault()
     console.log('testest')
 
-    // const user = this.state
+    const user = this.state.lawyer
 
-    // console.log(user)
+    console.log(user)
 
-    // axios.post(`http://localhost:3030/infolawyer`)
-    //   .then(res => {
-    //     console.log(res)
-    //     console.log(res.data)
-    //   })
+    axios.put(`http://localhost:3030/infolawyer`, { user })
+      .then(res => {
+        console.log(res)
+        console.log(res.data)
+      })
   }
 
   showUpdateForm = () => {
@@ -76,20 +68,20 @@ class Parameters extends React.Component {
             <div className="parameters-profile" style={{ display: this.state.displayInfo }}>
               <div>
                 <h2>Identifiants</h2>
-                <p className="parameters-p">Email&nbsp;: {this.state.email}</p>
-                <p className="parameters-p">Mot de passe&nbsp;: {this.state.password}</p>
+                <p className="parameters-p">Email&nbsp;: {this.state.lawyer.email}</p>
+                <p className="parameters-p">Mot de passe&nbsp;: {this.state.lawyer.password}</p>
               </div>
               <div>
                 <h2>Informations personnelles</h2>
-                <p className="parameters-p">Prénom&nbsp;: {this.state.firstName}</p>
-                <p className="parameters-p">Nom&nbsp;: {this.state.lastName}</p>
-                <p className="parameters-p">Cabinet&nbsp;: {this.state.cabinet}</p>
-                <p className="parameters-p">Numéro de téléphone&nbsp;: {this.state.phone}</p>
-                <p className="parameters-p">Adresse&nbsp;: {this.state.address}</p>
-                <p className="parameters-p">Ville&nbsp;: {this.state.city}</p>
-                <p className="parameters-p">Code Postal&nbsp;: {this.state.zipCode}</p>
-                <p className="parameters-p">Numéro de toque&nbsp;: {this.state.toque}</p>
-                <p className="parameters-p">Spécialité en droit&nbsp;: {this.state.field}</p>
+                <p className="parameters-p">Prénom&nbsp;: {this.state.lawyer.firstName}</p>
+                <p className="parameters-p">Nom&nbsp;: {this.state.lawyer.lastName}</p>
+                <p className="parameters-p">Cabinet&nbsp;: {this.state.lawyer.cabinet}</p>
+                <p className="parameters-p">Numéro de téléphone&nbsp;: {this.state.lawyer.phone}</p>
+                <p className="parameters-p">Adresse&nbsp;: {this.state.lawyer.address}</p>
+                <p className="parameters-p">Ville&nbsp;: {this.state.lawyer.city}</p>
+                <p className="parameters-p">Code Postal&nbsp;: {this.state.lawyer.zipCode}</p>
+                <p className="parameters-p">Numéro de toque&nbsp;: {this.state.lawyer.toque}</p>
+                <p className="parameters-p">Spécialité en droit&nbsp;: {this.state.lawyer.field}</p>
               </div>
               <div onClick={this.showUpdateForm}>
                 <Button>Modifier</Button>
@@ -99,27 +91,27 @@ class Parameters extends React.Component {
               <div className='form-parameters-container'>
                 <form className="form-parameters" onSubmit={this.HandleSubmit}>
                   <div className='form-div'>
-                    <input className='form-input-parameters' type="text" name="firstName" placeholder={this.state.firstName} id="firstName" onChange={this.UpdateField} />
-                    <input className='form-input-parameters' type="text" name="lastName" placeholder={this.state.lastName} id="lastName" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="firstName" value={this.state.lawyer.firstName} placeholder={this.state.lawyer.firstName} id="firstName" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="lastName" value={this.state.lawyer.lastName} placeholder={this.state.lawyer.lastName} id="lastName" onChange={this.UpdateField} />
                   </div>
                   <div className='form-div'>
-                    <input className='form-input-parameters' type="email" name="email" placeholder={this.state.email} id="email" onChange={this.UpdateField} />
-                    <input className='form-input-parameters' type="text" name="phone" placeholder={this.state.phone} id="phone" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="email" name="email" value={this.state.lawyer.email} placeholder={this.state.lawyer.email} id="email" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="phone" value={this.state.lawyer.phone} placeholder={this.state.lawyer.phone} id="phone" onChange={this.UpdateField} />
                   </div>
                   <div className='form-div'>
-                    <input className='form-input-parameters' type="text" name="cabinet" placeholder={this.state.cabinet} id="cabinet" onChange={this.UpdateField} />
-                    <input className='form-input-parameters' type="text" name="toque" placeholder={this.state.toque} id="toque" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="cabinet" value={this.state.lawyer.cabinet} placeholder={this.state.lawyer.cabinet} id="cabinet" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="toque" value={this.state.lawyer.toque} placeholder={this.state.lawyer.toque} id="toque" onChange={this.UpdateField} />
                   </div>
                   <div className='form-div'>
-                    <input className='form-input-parameters' type="text" name="address" placeholder={this.state.address} id="address" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="address" value={this.state.lawyer.address} placeholder={this.state.lawyer.address} id="address" onChange={this.UpdateField} />
                   </div>
                   <div className='form-div'>
-                    <input className='form-input-parameters' type="text" name="zipCode" placeholder={this.state.zipCode} id="zipCode" onChange={this.UpdateField} />
-                    <input className='form-input-parameters' type="text" name="city" placeholder={this.state.city} id="city" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="zipCode" value={this.state.lawyer.zipCode} placeholder={this.state.lawyer.zipCode} id="zipCode" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="text" name="city" value={this.state.lawyer.city} placeholder={this.state.lawyer.city} id="city" onChange={this.UpdateField} />
                   </div>
                   <div className='form-div'>
-                    <select className='form-select-parameters' name="field" placeholder={this.state.field} id="field" onChange={this.UpdateField} >
-                      <option value="" disabled selected>{this.state.field}</option>
+                    <select className='form-select-parameters' name="field" value={this.state.lawyer.field} placeholder={this.state.lawyer.field} id="field" onChange={this.UpdateField} >
+                      <option value="" disabled selected>{this.state.lawyer.field}</option>
                       <option value="1">droit2</option>
                       <option value="2">droit3</option>
                       <option value="3">droit4</option>
@@ -127,8 +119,8 @@ class Parameters extends React.Component {
                     </select>
                   </div>
                   <div className='form-div'>
-                    <input className='form-input-parameters' type="password" name="password" placeholder={this.state.password} id="password" onChange={this.UpdateField} />
-                    <input className='form-input-parameters' type="password" name="password" placeholder={this.state.password} id="passwordComfirm" />
+                    <input className='form-input-parameters' type="password" name="password" value={this.state.lawyer.password} placeholder={this.state.lawyer.password} id="password" onChange={this.UpdateField} />
+                    <input className='form-input-parameters' type="password" name="password" value={this.state.lawyer.password} placeholder={this.state.lawyer.password} id="passwordComfirm" />
                   </div>
                   <div>
                     <Button>Enregistrer</Button>
