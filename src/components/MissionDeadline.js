a964fd3cf2d3d4ecfcb80e0d0f9f3294d3e116d1
Baloulee <chinvalery@gmail.com>
2018-07-06 10:03:44 +0200

    date in comment

--- src/components/MissionDeadline.js
+++ src/components/MissionDeadline.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './style/MissionDeadline.css'
 
+
+//const sliceDate = (arg) => {return arg.slice(0,10)}
+
 const MissionDeadline = ({text}) => (
   <div>
-    <p className='mission-deadline'>{text}</p>
+    <p className='mission-deadline'>{/*sliceDate*/(text)}</p>
   </div>
 )
 export default MissionDeadline
