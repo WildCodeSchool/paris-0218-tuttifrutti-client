c06dbe22aa18c4e904db95b3378404580827ce07
Ludivine Pellissier <ludivine.pellissier@gmail.com>
2018-07-17 19:49:32 +0200

    mail confirm student, admin, lawyer OK with error messages

--- src/components/EmailConfirmAdmin.js
+++ src/components/EmailConfirmAdmin.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import HeaderSite from '../containers/HeaderSite.js'
 import PageTitle from './PageTitle.js'
 import axios from 'axios'
@@ -7,58 +7,72 @@ import './style/LoginForm.css'
 
 class EmailConfirm extends Component {
 
-    state = {
-        user: '',
-        response: ''
-    }
+	state = {
+		user: '',
+		response: ''
+	}
 
-    componentWillMount() {
+	componentWillMount() {
+		const queryString = window.location.pathname.split(`/`)
+		this.setState({ user: queryString[2] })
+	}
 
-        const queryString = window
-            .location
-            .pathname
-            .split(`/`)
-            this.setState({user: queryString[2]})
-            this.setState({mission: queryString[3]})
-        console.log(queryString)
-    }
+	componentDidMount() {
+		axios
+			.get(`http://localhost:3030/confirmationadmin/${this.state.user}`)
+			.then((response) => {
+				console.log(response)
+				this.setState({ response: response.data })
+			})
+	}
 
-    submit = () => {
-        console.log('trigger')
-        axios
-            .get(`http://localhost:3030/confirmationadmin/${this.state.user}`)
-            .then((response) => {
-                this.setState({response: response.data})
-						})
-						.then(console.log(this.state))
-    }
+	submit = () => {
+		window.location.replace('/loginadmin')
+	}
 
-    Blop = () => {
-        return <div><p>{this.state.response}</p></div>
-    }
+	mailVerified = () => {
+		return (
+			<div>
+				<p>
+					Votre inscription a bien été validée.
+						<br />
+					Vous pouvez désormais accéder à votre compte.
+					</p>
+				<div className='form-login-container'>
+					<button type="button" className='button' onClick={this.submit}>Se connecter</button>
+				</div>
+			</div>
+		)
+	}
 
-    render() {
-				console.log(this.state)
-        return (
-            <div>
-                <HeaderSite redirect='/'/>
-                <div className='login-content'>
-                    <div>
-                        <div className='title-login'>
-                            <PageTitle espace='Pour confirmer son compte, cliquer sur le boutton :'/>
-                        </div>
-                        <div>
-                            <div className='form-login-container'>
-                                <button type="button" className='button' onClick={this.submit}>Je confirme</button>
-                            </div>
-                        </div>
-                        {this.Blop}
-                    </div>
-                </div>
-                <Footer/>
-            </div>
-        )
-    }
+	invalidUser = () => {
+		return (
+			<div>
+				<p>Utilisateur introuvable.
+					<br />
+					Verifiez votre mail de confirmation.
+				</p>
+			</div>
+		)
+	}
+
+	render() {
+		return (
+			<div>
+				<HeaderSite redirect='/' />
+				<div className='login-content'>
+					<div>
+						<div className='title-login'>
+							<PageTitle espace='Bienvenue sur LITTA' />
+						</div>
+						{this.state.response === 'verified' ?
+							this.mailVerified() : this.invalidUser()}
+					</div>
+				</div>
+				<Footer />
+			</div>
+		)
+	}
 }
 
 export default EmailConfirm
